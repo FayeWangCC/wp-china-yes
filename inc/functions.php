963654b073372aaef800828e949f08cac04be41a
sunxiyuan <sxy@ibadboy.net>
2021-06-19 22:48:10 +0800

    修复：模板函数只能对同一个模板文件引入一次的问题

--- inc/functions.php
+++ inc/functions.php
@@ -238,9 +238,9 @@ function get_switch( string $name, string $value, string $type = 'advanced' ) {
  */
 function get_template_part( string $slug, string $name = '', array $tpl_args = array() ) {
     if ( empty( $name ) ) {
-        require_once WCY_ROOT_PATH . "template/{$slug}.php";
+        require WCY_ROOT_PATH . "template/{$slug}.php";
     } else {
-        require_once WCY_ROOT_PATH . "template/{$slug}-{$name}.php";
+        require WCY_ROOT_PATH . "template/{$slug}-{$name}.php";
     }
 }
 
